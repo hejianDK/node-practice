0bef01a19434418d0adb5e2a0fb4d831268c2c53
Zifan Zhang <zifan.zhang@finra.org>
2016-03-02 15:42:04 -0500

    separate data with another file

---
+++ app/js/data.js
@@ -0,0 +1,20 @@
+const names = [
+  {
+    "name": "n1...",
+    "status": "Powered on"
+  },
+  {
+    "name": "n2...",
+    "status": "Powered off"
+  },
+  {
+    "name": "n3...",
+    "status": "Powered on"
+  },
+  {
+    "name": "n4...",
+    "status": "suspended"
+  }
+];
+
+export default names;
--- app/main.js
+++ app/main.js
@@ -1,9 +1,9 @@
 import angular from 'angular';
 import './main.css';
+import names from './js/data';
 
 angular.module('mySwitch', [])
     .controller('switchCtrl', function ($scope) {
-        $scope.names = [{name: 'n1...', status: 'Powered on'}, {name: 'n2...', status: 'Powered off'},
-            {name: 'n3...', status: 'Powered on'}, {name: 'n4...', status: 'suspended'}];
+        $scope.names = names;
 
     });
